e17922c331eea3e29c050382aee64096947544d4
Cory Miller <corydhmiller@gmail.com>
2019-01-07 14:44:52 +0000

    Fiddle with some index stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Cory Miller" keywords={[`cory miller`, `front-end developer`, `gatsby`]} />
     <h1>Cory Miller</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Exploring front end development, creativity, and the ongoing balance of life, work, and family.</p>
     <Link to="/blog/">Go to page 2</Link>
   </Layout>
 )
