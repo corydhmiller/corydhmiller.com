0a356a2f1ad5c8ce12d0a8039d9fe45d19491a5c
Cory Miller <corydhmiller@gmail.com>
2021-11-23 09:39:36 +0000

    Remove further methods

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
 /* eslint-disable react/no-unescaped-entities */
 import Head from "next/head";
-import Header from "@components/Header";
 import Layout from "@components/Layout";
 import { Heading } from "@components/Typography";
-import { Paragraph } from "@components/Blocks/Blocks";
 import Content from "@components/Content";
-import SiteLink from "@components/UI/SiteLink";
 
-import { getAllPosts } from "../lib/posts";
 
 export default function Home({ posts }) {
 	return (
@@ -23,13 +19,3 @@ export default function Home({ posts }) {
 		</Layout>
 	);
 }
-
-export const getStaticProps = async () => {
-	const { posts } = await getAllPosts();
-
-	return {
-		props: {
-			posts,
-		},
-	};
-};
