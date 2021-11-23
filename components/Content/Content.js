606d3ebf468144f93aa07f6ea868b62fcc262114
Cory Miller <corydhmiller@gmail.com>
2021-11-23 07:54:19 +0000

    Add new content block and prep home

--- components/Content/Content.js
+++ components/Content/Content.js
@@ -1,9 +1,5 @@
 const Content = ({ children }) => {
-	return (
-		<div className="bg-blue-800">
-			<div className="max-w-4xl p-7 mx-auto text-gray-100">{children}</div>
-		</div>
-	);
+	return <div className="max-w-4xl p-7 mx-auto text-gray-100">{children}</div>;
 };
 
 export default Content;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable react/no-unescaped-entities */
 import Head from "next/head";
-import Header from "../components/Header";
-import Layout from "../components/Layout";
-import { Heading } from "../components/Typography";
-import Content from "../components/Content";
-import SiteLink from "../components/UI/SiteLink";
+import Header from "@components/Header";
+import Layout from "@components/Layout";
+import { Heading } from "@components/Typography";
+import Content from "@components/Content";
+import SiteLink from "@components/UI/SiteLink";
 
 import { getAllPosts } from "../lib/posts";
 
@@ -17,22 +17,15 @@ export default function Home({ posts }) {
 			<Header />
 			<Content>
 			<Heading level="h1">Cory's Thoughts</Heading>
-			<Heading level="h2">H2 Heading</Heading>
-			<Heading level="h3">H3 Heading</Heading>
-			<Heading level="h4">H4 Heading</Heading>
-			<Heading level="h5">H5 Heading</Heading>
-			<Heading level="h6">H6 Heading</Heading>
+
 
 				{posts.map((post) => (
 					<div key={post.postSlug} className="mb-8">
-						<Heading level="h1">
-							Henlo
-						</Heading>
-						<Heading level="h2">
+						<Heading level="h3">
 							<SiteLink href={`/blog/` + post.postSlug}>{post.postTitle}</SiteLink>
 						</Heading>
-						<Heading level="h4" className="text-lg font-semibold">{post.publishedBy.name}</Heading>
 						<p>{post.postContent}</p>
+						<p><SiteLink href={`/blog/` + post.postSlug}>Read on...</SiteLink></p>
 					</div>
 				))}
 			</Content>
