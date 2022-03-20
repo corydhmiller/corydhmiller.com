08722e84481c8450de225e4a000184ea89eac517
Cory Miller <corydhmiller@gmail.com>
2022-03-20 09:45:37 +0100

    Refactor index

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/no-unescaped-entities */
+import Head from "next/head"
+import Header from "@components/Header"
+import Layout from "@components/Layout"
+import { Heading } from "@components/Typography"
+import { Paragraph } from "@components/Blocks/Blocks"
+import Content from "@components/Content"
+
+interface HomeProps {
+	posts: {}
+}
+
+export default function Home({ posts }: HomeProps) {
+	return (
+		<Layout>
+			<Head>
+				<title>Cory&apos;s Thoughts</title>
+			</Head>
+			<Content>
+				<Heading level="h1">Rebuilding this. See you soon.</Heading>
+			</Content>
+		</Layout>
+	)
+}
