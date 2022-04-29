a65b40e4f789add1da3aa3ee46325cf0d921c711
Cory Miller <corydhmiller@gmail.com>
2022-04-29 15:06:53 +0200

    TSX changes

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,13 @@
-
-import Header from "@components/Header";
-
+import Header from "@components/Header"
 
 const Layout = ({ children }) => {
 	return (
 		<>
-			<div className="w-100vw p-6">
+			<div className="p-6 w-100vw">
 				<Header />
 				<main>{children}</main>
 			</div>
 		</>
-	);
-};
-export default Layout;
+	)
+}
+export default Layout
