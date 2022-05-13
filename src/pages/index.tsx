603baf08cc41c1cd702397da226d63132f93d4e1
Cory Miller <corydhmiller@gmail.com>
2022-05-13 06:53:11 +0200

    remove full content from homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,7 @@ import { Heading } from "@components/Typography"
 import { Paragraph } from "@components/Blocks"
 import Content from "@components/Content"
 import { getAllPosts } from "@lib/posts"
-import {MDXRemote} from "next-mdx-remote"
-import components from "@components/MDXComponents"
+import Link from "next/link"
 
 interface HomeProps {}
 
@@ -17,17 +16,17 @@ export default function Home({ posts }) {
 				<title>Cory&apos;s Thoughts</title>
 			</Head>
 			<Content>
-				<Heading as="h1">Thoughts</Heading>
+				<Heading as="h1">Cory&apos;s Thoughts</Heading>
 			</Content>
 			<Content>
-				{posts.map( (post) => {
+				{posts.map((post) => {
 					const { data, path, html } = post
 
 					return (
 						<div key={path}>
 							<Heading>{data.title}</Heading>
 							<Paragraph>{data.date}</Paragraph>
-							<MDXRemote {...html} components={components} />
+							<Link href={path}>Read me</Link>
 						</div>
 					)
 				})}
