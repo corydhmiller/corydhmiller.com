c556d535b2b2435b148aadb144958a9af4725307
Cory Miller <corydhmiller@gmail.com>
2023-10-30 10:53:31 +0100

    Fix some config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
-const withMDX = require('@next/mdx')()
- 
+const withMDX = require("@next/mdx")()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions`` to include MDX files
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
+	// Configure `pageExtensions`` to include MDX files
+	pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+
+	async redirects() {
+		return [
+			{
+				source: "/blog",
+				destination: "/",
+				permanent: true,
+			},
+		]
+	},
 }
- 
+
 module.exports = withMDX(nextConfig)
