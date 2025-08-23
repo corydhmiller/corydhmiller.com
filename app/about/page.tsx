109d5366367a01378cb36564638a06a63bc16ff1
Cory Miller <corydhmiller@gmail.com>
2025-08-23 16:24:26 +0200

    update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -40,11 +40,55 @@ export default function About() {
 					<h2 className="text-5xl font-bold">What I Do</h2>
 					<ul className="list-disc list-inside">
 						<li>
+							I take pictures of people around the world, streets photography
+							and portraits.
+						</li>
+						<li>
+							I write and produce music under the band{" "}
+							<Link
+								href="https://open.spotify.com/artist/5Xrdve5OCxSHoNZDUWCAN0?si=yXzv41aGS_ydhr-GJUk_4Q"
+								variant="primary"
+								newTab
+							>
+								Season Cove
+							</Link>
+							.
+						</li>
+						<li>
 							I work at{" "}
 							<Link href="https://kit.com" variant="primary" newTab>
 								Kit
 							</Link>{" "}
 							where I run the marketing site as the Lead Web Engineer.
+						</li>
+					</ul>
+
+					<h2 className="text-5xl font-bold">What I Use</h2>
+					<p>
+						I don't have any brand loyalty or affiliations, all of the cameras
+						and technology I use is from years of trial and error, or just for
+						the sake of fun.
+					</p>
+					<p>Here's a list of the equipment I use:</p>
+					<ul className="list-disc list-inside">
+						<li>
+							Cameras
+							<ul className="list-disc list-inside ml-4">
+								<li>Sony A7C</li>
+								<li>Fujifilm X-Pro 2</li>
+								<li>Canon AE-1 Program</li>
+								<li>Minolta SRT-101</li>
+								<li>Yashica Electro 35</li>
+							</ul>
+						</li>
+						<li>
+							Lenses
+							<ul className="list-disc list-inside ml-4">
+								<li>Sony FE 40mm f/2.5 G</li>
+								<li>Sony FE 85mm f/1.8</li>
+								<li>Fujifilm XF 23mm f/1.4 R</li>
+								<li>Fujifilm XF 35mm f/1.4 R</li>
+							</ul>
 						</li>
 					</ul>
 
@@ -75,7 +119,7 @@ export default function About() {
 }
 
 export const metadata = {
-	title: "About",
+	title: "About Me",
 	description: "Learn more about Cory Miller, photographer and web engineer.",
 	openGraph: {
 		title: "About - Cory Miller",
