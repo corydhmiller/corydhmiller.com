08dd2adbae5d19ae94c7597db20a56fc3b2ca55b
Cory Miller <corydhmiller@gmail.com>
2025-08-25 13:43:06 +0200

    update images

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -39,6 +39,6 @@ export const metadata: Metadata = {
 		url: "https://corydhmiller.com/photography",
 		siteName: "Cory Miller",
 		locale: "en_US",
-		images: [{ url: "https://corydhmiller.com/og?title=Photography+by+Cory" }],
+		images: [{ url: "https://corydhmiller.com/og?title=Photography+by+Cory&image=https://corydhmiller.com/images/gallery-background.jpg" }],
 	},
 }
