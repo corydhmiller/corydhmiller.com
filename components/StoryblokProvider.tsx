7d36e2d7b9d6330a3e63d47c142949c1d5669c73
Cory Miller <corydhmiller@gmail.com>
2025-08-29 14:35:17 +0200

    update the preview variables (#50)

--- components/StoryblokProvider.tsx
+++ components/StoryblokProvider.tsx
@@ -1,17 +1,12 @@
 "use client";
 import { initStoryblokClient } from "@/src/lib/storyblok-client";
-import { useEffect } from "react";
 
-const isVisualEditor = typeof window !== 'undefined' &&
+const isVisualEditor =
+  typeof window !== 'undefined' &&
   window.self !== window.top &&
   window.location.search.includes("_storyblok");
 
 export const StoryblokProvider = ({ children }: React.PropsWithChildren) => {
-  useEffect(() => {
-    if (isVisualEditor) {
-      initStoryblokClient();
-    }
-  }, []);
-
+  if (isVisualEditor) initStoryblokClient();
   return <>{children}</>;
 };
