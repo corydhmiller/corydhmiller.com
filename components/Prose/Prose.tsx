a402d0359f49ddc6cfdb16f489c740b733ff1993
Cory Miller <corydhmiller@gmail.com>
2025-08-30 12:40:58 +0200

    Change how articles and prose appear on the blog (#55)
    * change the preview look
    
    * update how prose looks

--- components/Prose/Prose.tsx
+++ components/Prose/Prose.tsx
@@ -11,10 +11,7 @@ const Prose = ({
 	return (
 		<div
 			className={cn(
-				"prose-2xl prose-pre:bg-transparent",
-				"prose-headings:text-gray-800 dark:prose-headings:text-white",
-				"prose-strong:text-gray-800 dark:prose-strong:text-white",
-				"prose-h1:text-4xl sm:prose-h1:text-8xl text-gray-800 dark:text-white text-balance",
+				"prose-xl prose-pre:bg-transparent prose-p:max-w-3xl prose-headings:text-gray-800 dark:prose-headings:text-white prose-strong:text-gray-800 dark:prose-strong:text-white prose-h1:text-3xl sm:prose-h1:text-8xl text-gray-800 dark:text-white text-balance prose-figcaption:text-sm sm:prose-figcaption:text-base prose-figcaption:italic prose-img:max-w-3xl prose-img:w-full",
 				className
 			)}
 		>
