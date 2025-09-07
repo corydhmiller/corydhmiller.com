3d7c532ea874267fa4e618b7f3b9948b6e0edbee
Cory Miller <cory@kit.com>
2025-09-07 10:52:25 +0200

    Remove qualities line from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,8 @@
-const withMDX = require("@next/mdx")()
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-	// Configure `pageExtensions`` to include MDX files
-	pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-	// Add images.unsplash.com to the image domains
-	images: {
-		remotePatterns: [
-			{ protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
-			{ protocol: "https", hostname: "a.storyblok.com", pathname: "/**" },
-		],
-	},
+  images: {
+    // Removed the qualities line
+    domains: ['example.com'],
+  },
+};
 
-	compiler: {
-		styledComponents: true,
-	},
-	images: {
-		qualities: [50],
-	},
-}
-
-module.exports = withMDX(nextConfig)
+module.exports = nextConfig;
