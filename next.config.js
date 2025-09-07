bcef880d568ab80005cd69bb378975cf035752af
Cory Miller <corydhmiller@gmail.com>
2025-09-07 12:31:14 +0200

    reset config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-module.exports = {
-  // other configurations
-  images: {
-    domains: ['example.com'],
-  },
-};
+const withMDX = require("@next/mdx")()
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	// Configure `pageExtensions`` to include MDX files
+	pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+	// Add images.unsplash.com to the image domains
+	images: {
+		remotePatterns: [
+			{ protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
+			{ protocol: "https", hostname: "a.storyblok.com", pathname: "/**" },
+		],
+	},
+
+	compiler: {
+		styledComponents: true,
+	},
+}
+
+module.exports = withMDX(nextConfig)
